620a8026b439e77c9e583de759bd697e6a1555eb
“Ivona” <ivona.rieger@gmail.com>
2025-04-12 14:53:27 +0200

    added procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r!-1d6)rxbqsv)4h#))a)$p8eedqu&a9+ix_1jmm2uhclzi1r!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+
 
 # Application definition
 
