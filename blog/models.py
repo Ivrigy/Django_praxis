00bf6fdcf12aff9c567e6a055a8625f4cb97d721
“Ivona” <ivona.rieger@gmail.com>
2025-04-22 13:30:37 +0200

    add i am so annoyed by this

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User # Import models to connect
+from django.contrib.auth.models import User  # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
 class Post(models.Model):
@@ -18,16 +19,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
-    
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -37,4 +36,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-    
